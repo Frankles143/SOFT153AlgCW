7b26ab5f00c2fe107112c49b74aae3a86e066dbc
Frankles143 <josh-franklin-@hotmail.co.uk>
2019-03-21 12:30:55 +0000

    Created classes

---
+++ Double Linked List/Double Linked List/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//Author: Josh Franklin
+//Created: 21/03/19
+//Edited: JF 21/03/19
+//
+//Creating a double linked list and adding functionality to traverse it both ways and sort
+
+namespace Double_Linked_List
+{
+    //Node class with references to the next and previous nodes in the list
+    class Node
+    {
+        public int data;
+        public Node nextNode;
+        public Node prevNode;
+    }
+
+    //Creating the list with the first node being saved
+    class List
+    {
+        public Node firstNode;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+            Node node; 
+
+        }
+    }
+}
